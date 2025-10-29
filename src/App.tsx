a6f89201eeb50d5f09c87fe3ae901b8498fe303f
Alessio Calore <alessio.calore96@gmail.com>
2025-10-29 22:34:01 +0100

    js analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
-import ReactGA from "react-ga4";
-import { useEffect } from "react";
+
 function App() {
-  ReactGA.initialize("G-2FKDTHJHR4");
-  useEffect(() => {
-    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
-  }, []);
   return (
     <>
       <BrowserRouter>
